d19daf50a74a3e1124dfb288ab8b0fa1395fefc0
Michael Woc <mwoc@seas.upenn.edu>
2019-10-31 19:40:39 +0000

    Upload New File

---
+++ corner_detector.py
@@ -0,0 +1,23 @@
+'''
+  File name: corner_detector.py
+  Author:
+  Date created:
+'''
+
+'''
+  File clarification:
+    Detects corner features in an image. You can probably find free “harris” corner detector on-line, 
+    and you are allowed to use them.
+    - Input img: H × W matrix representing the gray scale input image.
+    - Output cimg: H × W matrix representing the corner metric matrix.
+'''
+import cv2
+import numpy as np
+def corner_detector(img):
+    
+  # Calcuate Corners  
+  cimg = cv2.cornerHarris(img,2,3,0.04)
+  # Dilate Corners
+  cimg = cv2.dilate(cimg,None)
+    
+  return cimg

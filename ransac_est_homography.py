ad7c7e330cbcdbd58222ed603185670640b38417
Michael Woc <mwoc@seas.upenn.edu>
2019-11-07 01:53:58 +0000

    Replace ransac_est_homography.py

--- ransac_est_homography.py
+++ ransac_est_homography.py
@@ -1,7 +1,7 @@
 '''
   File name: ransac_est_homography.py
-  Author:
-  Date created:
+  Author: Brian Barrows, Zachary Fisher, Michael Woc
+  Date created: 11/3/2019
 '''
 
 '''
@@ -16,26 +16,26 @@
 '''
 import numpy as np
 import random
-from est_homography import *
+from est_homography import est_homography
 
 def ransac_est_homography(x1, y1, x2, y2, thresh):
   # Desired Number of Feature Pairs
   nPairs = 4
   # Desired Number of Iterations
   nRANSAC = 1000
-  # Minimum Consensus
-  min_Consensus = 10
-  # Max Correspondence Error
-  max_Error = 0.5
   # Generate Indexes
-  pairIndex = list(range(0,len(x1)))
-  im1_pts = np.zeros((nPairs,2))
-  im2_pts = np.zeros((nPairs,2))
-  inlier_ind = []
+  pairIndex = list(range(len(x1)))
+  # T/F Vector for Inlier or Outlier, temporary
+  inlier_indT = []
+  # Index of Inlier Point
+  inlier_idx = [] 
+  # Number of Inliers
+  nInliers = 0 
   for i in range(nRANSAC):
       # Choose 4 Random Indexes for Feature Pairs
       match_Sample = random.sample(pairIndex,nPairs)
-      for j in range(0,nPairs):
+      # range(0,nPairs)
+      for j in range(nPairs):
           k = match_Sample[j]
           im1_x = x1[k]
           im1_y = y1[k]
@@ -45,23 +45,46 @@ def ransac_est_homography(x1, y1, x2, y2, thresh):
       H_Est = est_homography(im1_x,im1_y,im2_x,im2_y)
       
       # Compute Inliers
-            
-      for m in range(len(im1_pts)):
-          im1_pt = np.array(im1_x[m], im1_y[m], 1).T # Transpose Image 1 Points
+      for m in range(len(x1)):
+          # Transpose Image 1 Point
+          im1_pt = np.array(im1_x[m], im1_y[m], 1).T
+          # Combine Image 2 Point Coordinates
           im2_pt = np.array(im2_x[m], im2_y[m])
-          q_T1 = np.dot(H_Est,im1_pt)
-          norm_1 = np.array(q_T1[0]/q_T1[2], q_T1[1]/q_T1[2])
-           
-    # Calculate Distances and Error and Compare to Threshold, must be less than threshold
-          if np.linalg.norm(norm1-im2_pt) <= thresh:
-              inlier_ind.append(m)
-      
-      
+          # Transform Image 1 Point
+          im1_T = np.dot(H_Est,im1_pt) 
+          # Convert Homogenous Coordinates to X,Y coordinates
+          norm1 = np.array(im1_T[0]/im1_T[2], im1_T[1]/im1_T[2]) 
+          
+          # Calculate Distances and Error and Compare to Threshold, 
+          # Must be less than or eqaul to threshold
+          if (np.linalg.norm(norm1-im2_pt) <= thresh):
+              inlier_idx.append(m) # Save index of inlier
+              inlier_indT.append(1) # Denotes inlier
+          else:
+              inlier_indT.append(0) # Denotes outlier
+              
+      # If number of inliers is greater than before, update inliers list
+      if (len(inlier_idx) > nInliers):
+          inliers = inlier_idx # Save Inlier Index
+          inlier_ind = inlier_indT # Save T/F Vector for Output
+          nInliers = len(inlier_idx) # Update Current Number of Inliers
+          
+      # If number of inliers is less than 10, then increase iterations
+      if ((i==nRANSAC) and (nInliers==9)):
+          nRANSAC += 10
+          print("Number of Iterations Increased, Not Enough Inliers")
+          
+     # print("Iteration Number:", i) # Keep Track of Number of Iterations
+  
+  # Get Coordinates for Inliers
+  for i in range(len(inliers)):
+      j = inliers[i]
+      im1_xF = x1[j]
+      im1_yF = y1[j]
+      im2_xF = x2[j]
+      im2_yF = y2[j]
+  
   # Compute Homography with Inliers
-  for i in range(len(inlier_ind)):
-
-      for j in range(0,nPairs):
-          k = match_Sample[j]
-      # Estimate Homography 
-      H = est_homography(x1[k],y1[k],x2[k],y2[k])
+  H = est_homography(im1_xF,im1_yF,im2_xF,im2_yF)
+  
   return H, inlier_ind

6836b128a535d64ce583b79819a7dd6cb7fef0d6
barrowsb <barrows.b7@gmail.com>
2019-11-07 17:55:40 +0000

    Replace anms.py

--- anms.py
+++ anms.py
@@ -19,9 +19,15 @@ import numpy as np
 
 def anms(cimg, max_pts):
     
+    # Tune for thresholding
+    thresh1 = 0.2 # pre-threshold
+    thresh2 = 1.2 # anms-threshold
+    
+    # %% Pre-thresholding
+    
     # Create meshgrids and flatten
     nr,nc = cimg.shape
-    cols,rows = np.meshgrid(range(nc),range(nr)) 
+    cols,rows = np.meshgrid(range(nc),range(nr))
     colsf = cols.flatten()
     rowsf = rows.flatten()
     cimgf = cimg.flatten()
@@ -35,21 +41,54 @@ def anms(cimg, max_pts):
     colsf = list(colsf)
     
     # Threshold
-    thresh = 0.2*cimgf[0]
-    cimgf[:] = [ele if ele > thresh else 0 for ele in cimgf]
+    prethresh = thresh1*cimgf[0]
+    #cimgf[:] = [ele if ele > prethresh else 0 for ele in cimgf]
+    cimgf = list(np.where(cimgf>prethresh, np.asarray(cimgf), np.zeros((nr*nc,))))
     cimgf = np.trim_zeros(cimgf)
     length = len(cimgf)
     rowsf = rowsf[0:length]
     colsf = colsf[0:length]
     
-    # Find distance to nearest corner with higher score (1-1.2x as sure)
+    # %% ANMS
     
+    # Initialize variables
+    # (for matrices: row index is reference corner, column index is corner being compared to)
+    inf = (nr**2)+(nc**2) # longest possible distance^2
+    greater = np.zeros((length,length),dtype=bool) # Boolean matrix (1 if above anms-thresh)
+    dist2 = np.zeros((length,length)) # distance^2 matrix (from ref to compare corner)
+    
+    # Find all corners above anms-threshold
+    ref,compare = np.meshgrid(cimgf,cimgf) # row-wise and column-wise matrices of cimgf vectors
+    anmsthresh = ref*thresh2
+    greater = compare > anmsthresh
+    
+    # Compute distance from all ref corners to all other corners
+    rows_comp,cols_comp = np.meshgrid(rowsf,colsf) # row-wise and column-wise matrices of indices
+    rows_ref = np.transpose(rows_comp) # (rows_ref does not change row-wise, rows_comp does)
+    cols_ref = np.transpose(cols_comp) # (cols_ref does not change row-wise, cols_comp does)
+    dist2 = (rows_ref - rows_comp)**2 + (cols_ref - cols_comp)**2
+
+    # Find minimum distance (read: radius of separation) where true in Boolean matrix (greater)
+    logical_dist2 = np.where(greater,dist2,np.ones((length,length))*inf) # inf where not(greater)
+    j_min = np.argmin(logical_dist2,axis=1) # col (j) indices of row-wise minimum
+    i_min = np.linspace(0,length-1,length,dtype=int) # row (i) indices
+    min_dist2 = dist2[i_min,j_min] # minimum distance to (anms-thresheld) larger corner
+    
+    # Find row and col indices of minimum distance from above
+    min_rows = np.asarray(rowsf)[j_min]
+    min_cols = np.asarray(colsf)[j_min]
     
     # Sort by decending radius
+    z2 = zip(min_dist2,min_rows,min_cols)
+    z2 = sorted(z2, key=lambda x: x[0],reverse=True)
+    min_dist2_sorted,min_rows_sorted,min_cols_sorted = zip(*list(z2))
+    min_dist2_sorted = list(min_dist2_sorted)
+    min_rows_sorted = list(min_rows_sorted)
+    min_cols_sorted = list(min_cols_sorted)
     
-    
-    # Trim to N = max_pts corners
-    
-    
+    # Set outputs, trimmed to N = max_pts corners
+    x = min_cols_sorted[0:max_pts]
+    y = min_rows_sorted[0:max_pts]    
+    rmax = min_dist2_sorted[max_pts]
     
     return x, y, rmax

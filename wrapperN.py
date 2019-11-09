c84edd2d2c8cc936f3ea9d2c0701b6b6f106b4ee
Zachary Fisher <zjf219@seas.upenn.edu>
2019-11-09 20:01:04 +0000

    Replace wrapperN.py

--- wrapperN.py
+++ wrapperN.py
@@ -21,9 +21,17 @@ from ransac_est_homography import *
 from mymosaic import *
 
 # Import Images
-imgL = cv2.imread('left.jpg')
-imgM = cv2.imread('middle.jpg')
-imgR = cv2.imread('right.jpg')
+#imgL = cv2.imread('left.jpg')
+#imgM = cv2.imread('middle.jpg')
+#imgR = cv2.imread('right.jpg')
+
+#imgL = cv2.imread('eng_left.jpg')
+#imgM = cv2.imread('eng_middle.jpg')
+#imgR = cv2.imread('eng_right.jpg')
+
+imgL = cv2.imread('new_left.jpg')
+imgM = cv2.imread('new_middle.jpg')
+imgR = cv2.imread('new_right.jpg')
 
 # Grayscale
 grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
@@ -34,45 +42,51 @@ grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 cimgL = corner_detector(grayL)
 cimgM = corner_detector(grayM)
 cimgR = corner_detector(grayR)
-print('corner_detector')
 
 # Adaptive Non-Maximal Suppression
 max_pts = 100
 xL,yL,rmaxL = anms(cimgL, max_pts)
 xM,yM,rmaxM = anms(cimgM, max_pts)
 xR,yR,rmaxR = anms(cimgR, max_pts)
-print('anms')
+
+print('leftX')
+print(xL)
+print('rightX')
+print(yL)
+print('midX')
+print(xM)
+print('midY')
+print(yM)
 
 # Feature Descriptors
 descsL = feat_desc(grayL, xL, yL)
 descsM = feat_desc(grayM, xM, yM)
 descsR = feat_desc(grayR, xR, yR)
-print('feat_desc')
 
 # Feature Matching
 matchL = feat_match(descsM, descsL)
 matchR = feat_match(descsM, descsR)
-print('feat_match')
 
 # Get feature coordiantes
+x1ML = []
+y1ML = []
+x2L = []
+y2L = []
+x1MR = []
+y1MR = []
+x2R = []
+y2R = []
 for i in range(len(matchL)):
-    if (matchL(i) != -1):
-        x1ML = xM[matchL(i)]
-        y1ML = yM[matchL(i)]
-        x2L = xL[matchL(i)]
-        y2L = xL[matchL(i)]
-    else:
-        print('placeholder')
-        
-for i in range(len(matchR)):
-    if (matchR(i) != -1):
-        x1MR = xM[matchL(i)]
-        y1MR = yM[matchL(i)]
-        x2R = xR[matchR(i)]
-        y2R = xR[matchR(i)]
-    else:
-        print('placeholder')
-print('feat coords')
+    if (matchL[i] != -1):
+        x1ML.append(xM[int(matchL[i])])
+        y1ML.append(yM[int(matchL[i])])
+        x2L.append(xL[int(matchL[i])])
+        y2L.append(xL[int(matchL[i])])
+    if (matchR[i] != -1):
+        x1MR.append(xM[int(matchL[i])])
+        y1MR.append(yM[int(matchL[i])])
+        x2R.append(xR[int(matchR[i])])
+        y2R.append(xR[int(matchR[i])])
 
 # RAndom Sampling Consensus (RANSAC)
 threshL = 0.5
